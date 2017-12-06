26a0c7847b7ce2b4659403ca05d1895c318a1ef7
Riccardo Ancona <riccardo.ancona@gmail.com>
2017-12-06 08:20:54 +0000

    Added new feature

--- yellowFood.py
+++ yellowFood.py
@@ -7,9 +7,15 @@ trello=TrelloApi(TRELLO_APP_KEY)
 
 print trello.get_token_url('YellowFood', expires='90days', write_access=True) 
 
-user_token='3632e23297d6255b31eca3bfaf3629c37f2f01c8e6e1f08d3c40d2672448af72'
+user_token='cbecb81059cebb2d026afa70977c99834a51b4c53343f4cbc280a0ad69549943'
 
 trello.set_token(user_token)
 
-data = json.loads(trello.boards.get('5a0068aabd9f13a3c7e28b7e'))
+boards = trello.boards.get('5a0068aabd9f13a3c7e28b7e')
+
+print boards['name']
+print boards['labelNames']
+
+
+print data
 
