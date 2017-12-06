1a69e358585a98c6ceff33057fcef22e08eafc97
Riccardo Ancona <riccardo.ancona@gmail.com>
2017-12-06 07:56:07 +0000

    Fixed connection to trello board

--- yellowFood.py
+++ yellowFood.py
@@ -1,15 +1,15 @@
-# b730b6c72f876470a8c4cbf5ebe41dc0
-
 from trello import TrelloApi
+import json
 
 TRELLO_APP_KEY='b730b6c72f876470a8c4cbf5ebe41dc0'
 
 trello=TrelloApi(TRELLO_APP_KEY)
 
-user_token=trello.get_token_url('YellowFood', expires='30days', write_access=True) 
-#   'https://trello.com/1/authorize?key='+TRELLO_APP_KEY+'&name=My+App&expiration=30days&response_type=token&scope=read,write'
+print trello.get_token_url('YellowFood', expires='90days', write_access=True) 
+
+user_token='3632e23297d6255b31eca3bfaf3629c37f2f01c8e6e1f08d3c40d2672448af72'
 
 trello.set_token(user_token)
 
-trello.boards.list(TRELLO_APP_KEY, )
-trello.boards.get('Yellow Meal Planning Board')
+data = json.loads(trello.boards.get('5a0068aabd9f13a3c7e28b7e'))
+
