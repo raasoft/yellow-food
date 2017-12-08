4535fed17b16e59ae8aaacd6ed71e7da83c9dbee
Riccardo Ancona <riccardo.ancona@gmail.com>
2017-12-08 19:36:47 +0000

    Updated

--- yellowFood.py
+++ yellowFood.py
@@ -16,6 +16,19 @@ boards = trello.boards.get('5a0068aabd9f13a3c7e28b7e')
 print boards['name']
 print boards['labelNames']
 
+lists = trello.boards.get_list('5a0068aabd9f13a3c7e28b7e')
+cards = trello.boards.get_card('5a0068aabd9f13a3c7e28b7e')
 
-print data
+
+for l in lists:
+    print l['name']
+
+
+for c in cards:
+    print c['name']
+    print c['due']
+
+print cards[0]
+
+print cards[0]['due']
 
